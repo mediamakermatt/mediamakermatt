a27311cd1a3b4e5dd77ca763a6b165341027f85a
mediamakermatt <mediamakermatt@gmail.com>
2020-10-15 16:23:52 -0400

    wordpress index now at root

--- content/framework/primordial.php
+++
@@ -1,2 +0,0 @@
-Hello Primordial
-<?php echo "SDFDSF"; ?>
--- index.php
+++ index.php
@@ -1,27 +1,17 @@
-<?php echo "Hello" ?>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<!DOCTYPE html>
-<html lang="en-US">
-    <head>
-        <meta charset="UTF-8">
-        <!-- <link rel="stylesheet" type="text/css" href="content/framework/primordial.css"> -->
-        <title>MediaMakerMatt</title>
-    </head>
-    <body>
-        <!-- START CONTENT -->
-        <div class="row">
-            <div class="col-50">
-                Bootstrap Column 7
-            </div>
-            <div class="col-50">
-                Bootstrap Column 6
-            </div>
-        </div>
-        <!-- END CONTENT -->
-    </body>
-</html>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-
-
-
-
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
