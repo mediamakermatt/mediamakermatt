d09d2d4cb29fa48b0005cfd23c2a3d41b8864367
mediamakermatt <mediamakermatt@gmail.com>
2020-10-26 11:55:36 -0400

    attempt to make production database

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,16 @@
 define( 'DB_NAME', 'mediamk0_mediamakermatt' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+// define( 'DB_USER', 'root' );
+define( 'DB_USER', 'mediamk0_mediamakermatt' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+// define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Or!ch1maru20XX' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+// define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhost:3306' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
